567e913933cb14b0243ad3446ee1c2643daab5f9
Amidro <moonlight9212@gmail.com>
2026-04-24 15:04:59 +0500

    Update page.tsx

--- app/ru/intraokulyarnye-linzy/page.tsx
+++ app/ru/intraokulyarnye-linzy/page.tsx
@@ -1,10 +1,8 @@
-import Header from "../../../components/Header";
-import Footer from "../../../components/Footer";
 import {
-  DropIcon,
   EyeIcon,
-  LayersIcon,
-  LensIcon
+  IntermediateIcon,
+  LensIcon,
+  MultifocalIcon
 } from "../../../components/icons";
 
 const lenses = [
