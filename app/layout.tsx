172f953490a3a1773706b8db894b96b49b53a083
Amidro <moonlight9212@gmail.com>
2026-04-29 15:22:25 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,6 +57,25 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ru">
       <body>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6Y5BZYBYFY"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-6Y5BZYBYFY');
+            `
+          }}
+        />
+
+        {/* Yandex.Metrika */}
         <Script
           id="yandex-metrika"
           strategy="afterInteractive"
